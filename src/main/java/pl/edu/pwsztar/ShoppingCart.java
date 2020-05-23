25cf949b33b8e312113243d617f31fed9474a824
Jakub Zegar <jakubzegar3@gmail.com>
2020-05-23 19:59:08 +0200

    Functionality implemented

--- src/main/java/pl/edu/pwsztar/ShoppingCart.java
+++ src/main/java/pl/edu/pwsztar/ShoppingCart.java
@@ -1,30 +1,33 @@
 package pl.edu.pwsztar;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ShoppingCart implements ShoppingCartOperation {
 
+    private Cart cart = new CartImpl();
+
     public boolean addProducts(String productName, int price, int quantity) {
-        return false;
+        return cart.addProductToCart(productName,quantity,price);
     }
 
     public boolean deleteProducts(String productName, int quantity) {
-        return false;
+        return cart.removeSomeAmountOfProductFromCart(productName,quantity);
     }
 
     public int getQuantityOfProduct(String productName) {
-        return 0;
+        return cart.getItem(productName).getAmount();
     }
 
     public int getSumProductsPrices() {
-        return 0;
+        return cart.getAllProductsFromCart().stream().mapToInt(total -> total.getPrice() * total.getAmount()).sum();
     }
 
     public int getProductPrice(String productName) {
-        return 0;
+        return cart.getItem(productName).getPrice();
     }
 
     public List<String> getProductsNames() {
-        return null;
+        return cart.getAllProductsFromCart().stream().map(Item::getName).collect(Collectors.toList());
     }
 }
